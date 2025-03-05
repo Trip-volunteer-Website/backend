c9a8dc0d3395ec624b2067eee103d280d07c5efb
nedaa2024 <nedaaalysein@gmail.com>
2025-03-06 02:04:45 +0300

    about-role-trip done repo+services+controller

---
+++ TripVolunteer/Controllers/AboutController.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TripVolunteer.API.Controllers;
+using TripVolunteer.Core.Data;
+using TripVolunteer.Core.Services;
+using TripVolunteer.Infra.Services;
+
+namespace TripVolunteer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AboutController : ControllerBase
+    {
+        private readonly IAboutService aboutService;
+        public AboutController(IAboutService aboutService)
+        {
+            this.aboutService = aboutService; 
+        }
+
+        [HttpGet]
+        public List<Staticabout>GetAllabout()
+        {
+            return aboutService.GetAll();
+        }
+
+        [HttpGet]
+        [Route("getbyid/{id}")]
+        public Staticabout GetAboutById(int id)
+        {
+            return aboutService.GetAboutById(id);
+
+        }
+
+        [HttpPost]
+        public void createAbout(Staticabout about) 
+        { 
+         aboutService.CreateAbout(about);
+        
+        
+        
+        }
+
+        [HttpPut]
+        public void updateAbout(Staticabout about)
+        {
+            aboutService.UpdateAbout(about);
+        }
+
+        [HttpDelete]
+        [Route("deleteabout/{id}")]
+        public void deleteAbout(int id)
+        {
+            aboutService.DeleteAbout(id);
+        }
+
+
+        [HttpPost]
+        [Route("UploudeImage")]
+        public Staticabout UploudeImage()
+        {
+            var file=Request.Form.Files[0];
+            var fileName=Guid.NewGuid().ToString()+"_"+file.FileName;
+            var fullPath=Path.Combine("Images",fileName);
+            using (var stream=new FileStream(fullPath,FileMode.Create))
+            { file.CopyTo(stream); }
+            Staticabout item=new Staticabout();
+            item.Img1path = fileName;
+            return item;
+        }
+    }
+}
--- TripVolunteer/Program.cs
+++ TripVolunteer/Program.cs
@@ -1,9 +1,16 @@
-using TripVolunteer.Core.Common;
+﻿using TripVolunteer.Core.Common;
+using TripVolunteer.Core.Repository;
+using TripVolunteer.Core.Services;
 using TripVolunteer.Infra.Common;
+using TripVolunteer.Infra.Repository;
+using TripVolunteer.Infra.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Configuration.AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                     .AddEnvironmentVariables();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -12,14 +19,29 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IDbContext, DbContext>();
 
 
-var app = builder.Build();
+builder.Services.AddScoped<IRoleRepository, RoleRepository>();
+builder.Services.AddScoped<IRoleService, RoleService>();
 
-// Configure the HTTP request pipeline.
+builder.Services.AddScoped<IAboutRepository, AboutRepository>();
+builder.Services.AddScoped<IAboutService, AboutService>();
+
+builder.Services.AddScoped<ITripRepository, TripRepository>();
+builder.Services.AddScoped<ITripService, TripService>();
+
+
+var app = builder.Build();
 if (app.Environment.IsDevelopment())
+
 {
+
     app.UseSwagger();
+
     app.UseSwaggerUI();
+
 }
+
+
+
 
 app.UseHttpsRedirection();
 
@@ -28,3 +50,13 @@ app.UseAuthorization();
 app.MapControllers();
 
 app.Run();
+
+
+
+
+
+
+
+
+
+
