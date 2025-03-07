661e8536630fe4a857418cbde3811f0d0b84b8d2
nedaa2024 <nedaaalysein@gmail.com>
2025-03-07 03:06:24 +0300

    add testimoinail controller

---
+++ TripVolunteer/Controllers/TestimonialController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using TripVolunteer.Core.Data;
+using TripVolunteer.Core.Services;
+using TripVolunteer.Infra.Services;
+
+namespace TripVolunteer.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestimonialController : ControllerBase
+    {
+        private readonly ITestimonialService testimonialService;
+        public TestimonialController(ITestimonialService testimonialService)
+        {
+            this.testimonialService = testimonialService;
+        }
+
+
+        [HttpGet]
+        public List<Testimonial> GetAllTestimonials()
+        {
+            return testimonialService.GetAllTestimonials();
+        }
+
+        [HttpPost]
+        public void makeTestimonial(Testimonial testimonial)
+            
+        {
+            testimonialService.makeTestimonial(testimonial);
+        }
+
+
+        [HttpPut]
+        public void UpdateTestimonial(Testimonial testimonial)
+        {
+           testimonialService.UpdateTestimonial(testimonial);
+        }
+
+        [HttpDelete]
+        [Route("Deletetestimonial/{id}")]
+        public void DeleteTestimonial(int id)
+        {
+            testimonialService.DeleteTestimonial(id);
+        }
+
+        [HttpGet]
+        [Route("TestimonialById/{id}")]
+        public Testimonial GetTestimonialById(int id)
+        {
+            return testimonialService.GetTestimonialById(id);
+        }
+
+
+
+
+        [HttpGet]
+        [Route("TestimonialCountByStatus")]
+        public List<Testimonial> GetTestimonialCountByStatus()
+        {
+            return testimonialService.GetTestimonialCountByStatus();
+        }
+
+
+
+
+        [HttpPut]
+        [Route("ApprovOrRejectTestimonial/{testimonialId}/{newStatus}")]
+        public void ApprovOrRejectTestimonial(int testimonialId, string newStatus)
+        {
+                   testimonialService.ApprovOrRejectTestimonial(testimonialId , newStatus);
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
